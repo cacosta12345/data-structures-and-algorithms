9f2683724fe5cbcb1d915f8c4039e7fbb2817666
chris acosta <chrisacosta12345@gmail.com>
2024-01-24 16:09:34 -0800

    passes tests

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -1,13 +1,30 @@
 from data_structures.queue import Queue
 
-
 class AnimalShelter:
-    pass
+    def __init__(self):
+        self.dogs = Queue()
+        self.cats = Queue()
 
+    def enqueue(self, animal):
+        if isinstance(animal, Dog):
+            self.dogs.enqueue(animal)
+        elif isinstance(animal, Cat):
+            self.cats.enqueue(animal)
+
+    def dequeue(self, pref):
+        if pref == "dog" and not self.dogs.is_empty():
+            return self.dogs.dequeue()
+        elif pref == "cat" and not self.cats.is_empty():
+            return self.cats.dequeue()
+        else:
+            return None
 
 class Dog:
-    pass
-
+    def __init__(self, name=""):
+        self.species = "dog"
+        self.name = name
 
 class Cat:
-    pass
+    def __init__(self, name=""):
+        self.species = "cat"
+        self.name = name
