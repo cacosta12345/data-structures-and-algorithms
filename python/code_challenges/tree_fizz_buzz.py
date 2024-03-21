bbe410634a3cfc2b4aeac015fc4492754a4561e4
Chris Acosta <chrisacosta12345@gmail.com>
2024-03-21 16:06:01 -0700

    updated readme

--- python/code_challenges/tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz.py
@@ -1,5 +1,28 @@
 from data_structures.binary_tree import BinaryTree
 
 
-def fizz_buzz_tree():
-    pass
+class TreeNode:
+    def __init__(self, value=None):
+        self.value = value
+        self.children = []
+
+def fizz_buzz_tree(root):
+    def fizz_buzz(value):
+        if value % 3 == 0 and value % 5 == 0:
+            return "FizzBuzz"
+        elif value % 3 == 0:
+            return "Fizz"
+        elif value % 5 == 0:
+            return "Buzz"
+        else:
+            return str(value)
+
+    def helper(node):
+        if not node:
+            return None
+        new_node = TreeNode(fizz_buzz(node.value))
+        new_node.children = [helper(child) for child in node.children]
+        return new_node
+
+    return helper(root)
+
